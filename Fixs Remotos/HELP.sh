08401d9fc72e2784c91e4aea0026f46742252b96
yosoyhendrix <hendrix.dominguez@gmail.com>
2025-07-23 21:56:53 -0400

    Add files via upload

---
+++ Fixs Remotos/HELP.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Soporte Remoto (HELP)
+echo "" >/dev/null 2>&1
+wget https://raw.githubusercontent.com/IntelectoDev/ElementManager/master/Fixs%20Remotos/SOPORTE.sh -O /usr/bin/SOPORTE > /dev/null 2>&1
+chmod +x /usr/bin/SOPORTE > /dev/null 2>&1
